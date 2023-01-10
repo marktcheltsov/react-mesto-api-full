d24567e95843203b0944bad725958e81ab1acb30
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2023-01-10 18:49:17 +0300

    Add files via upload

---
+++ backend/middlewares/cheackCors.js
@@ -0,0 +1,21 @@
+const allowedCors = ['http://cheltsovsmesto.nomoredomains.club'];
+
+const cheakCors = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const reqHeaders = req.headers['Access-Control-Request-Headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', reqHeaders);
+    return res.end();
+}
+  next();
+};
+
+module.exports = cheakCors;
