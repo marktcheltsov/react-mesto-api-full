0d072272d9c1be89acd9e4fc59215f023a1da791
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2023-01-12 12:45:15 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -25,16 +25,6 @@ const { auth } = require('./middlewares/auth');
 
 app.use(bodyParser.json());
 
-const allowedCors = ['http://cheltsovsmesto.nomoredomains.club', 'http://localhost:3000'];
-
-const corsOptions = {
-  origin: allowedCors,
-  optionsSuccessStatus: 200,
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
