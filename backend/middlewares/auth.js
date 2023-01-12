fccaa8aa8a7da9f4d399c4095f565dac74b58f86
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2023-01-12 13:54:35 +0300

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,12 +3,12 @@ const jwt = require('jsonwebtoken');
 const WrongData = require('../errors/wrong-data-err');
 
 const auth = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  const { Authorization } = req.headers;
+  if (!Authorization || !Authorization.startsWith('Bearer ')) {
     const err = new WrongData('Необходима авторизация');
     next(err);
   }
-  const token = authorization.replace('Bearer ', '');
+  const token = Authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
