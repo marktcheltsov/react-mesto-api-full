be568943322b9d72265c3d3514d8ad3bd7181744
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2023-01-23 22:27:45 +0300

    Delete backend directory

--- backend/errors/data-auth-err.js
+++
@@ -1,8 +0,0 @@
-class DataAuthErr extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 409;
-  }
-}
-
-module.exports = DataAuthErr;
--- backend/errors/wrong-data-err.js
+++
@@ -1,8 +0,0 @@
-class WrongData extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 401;
-  }
-}
-
-module.exports = WrongData;
