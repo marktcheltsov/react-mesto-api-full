8062d7d60ca90df8e8aac12e8f2deffce77b3332
marktcheltsov <104628734+marktcheltsov@users.noreply.github.com>
2023-01-23 22:37:40 +0300

    Add files via upload

---
+++ backend/models/users.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const { default: isEmail } = require('validator/lib/isEmail');
+const { default: isURL } = require('validator/lib/isURL');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    default: 'Жак-Ив Кусто',
+    minlength: 2,
+    maxlength: 30,
+  },
+
+  about: {
+    type: String,
+    default: 'Исследователь',
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (url) => isURL(url),
+      message: (url) => `${url} не прошел валидацию`,
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => isEmail(email),
+      message: (email) => `${email} не прошел валидацию`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+}, { bufferCommands: false });
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
+const {
+  getUsers, getUser, updateUser, updateUserAvatar,
+} = require('../controllers/user');
+
+router.get('/', getUsers);
+
+router.get('/me', getUser);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required(),
+  }),
+}), updateUserAvatar);
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUser);
+
+const userRouter = router;
+module.exports = userRouter;
